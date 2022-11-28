e073157c4bd9c220f301cc26e9b5aafd70e337c5
uTakCouDeT <b20030021@gmail.com>
2022-11-28 03:22:33 +0300

    aboba

--- kursa4/MySite/models.py
+++ kursa4/MySite/models.py
@@ -1,17 +1,54 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    title = models.CharField(max_length=70, verbose_name='Название')
+
+class VirtualMachine(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='images', verbose_name='Изображение')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-
-    TYPE = [
-        ('Lin', 'Linux'),
-        ('Win', 'Windows'),
+    ZONE = [
+        ('ru-central1-a', 'ru-central-a'),
+        ('ru-central1-b', 'ru-central-b'),
+        ('ru-central1-c', 'ru-central-c'),
     ]
+    zone = models.CharField(choices=ZONE, max_length=13, default='ru-central1-a', verbose_name='Зона доступности')
 
-    type = models.CharField(choices=TYPE, max_length=3, default='BRK', verbose_name='Тип')
+    IMAGE_ID = [
+        ('fd8bh0c781u19q50m4kj', 'Ubuntu-22.04'),
+        ('fd8e65d4chiel4ussa9o', 'Debian-11'),
+        ('fd84go2nk3voj0gg7tpn', 'openSUSE-Leap-15.3'),
+        ('fd8bv62nmsjf6hmjrh96', 'AlmaLinux-8'),
+        ('fd8nce10ece89ufqkk9t', 'Astra-Linux-Common-Edition-2.12'),
+        ('fd8i8fljrbbcclckhlm9', 'ALT-Linux-10'),
+        ('fd8jvcoeij6u9se84dt5', 'CentOS-7'),
+        ('fd8phu675e6sgsa68pvj', 'Fedora-35'),
+        ('fd865udut6b1gvgh5igh', 'Fedora-CoreOS-35'),
+        ('fd8mnkj7vk131q9ll4p5', 'CentOS-Stream-8'),
+        ('fd8q0kjl4l1iovds9f29', 'REDOS-7.3'),
+    ]
+    image_id = models.CharField(choices=IMAGE_ID, max_length=20, default='fd8phu675e6sgsa68pvj',
+                                verbose_name='Операционная система')
+
+    BOOT_DISK_TYPE = [
+        ('network-hdd', 'HDD'),
+        ('network-ssd', 'SSD'),
+        ('network-ssd-nonreplicated', 'NR-SSD'),
+    ]
+    boot_disk_type = models.CharField(max_length=25, choices=BOOT_DISK_TYPE, default='network-hdd', verbose_name='Тип диска')
+    boot_disk_size = models.IntegerField(verbose_name='Размер диска', default=15)
+
+    cores = models.IntegerField(verbose_name='vCPU', default=2)
+    CORE_FRACTION = [
+        (20, '20%'),
+        (50, '50%'),
+        (100, '100%'),
+    ]
+    core_fraction = models.IntegerField(choices=CORE_FRACTION, verbose_name='Гарантированная доля vCPU', default=20)
+    memory = models.IntegerField(verbose_name='RAM', default=2)
+
+    username = models.CharField(max_length=20, verbose_name='Имя пользователя')
+    ssh_key = models.CharField(max_length=500, verbose_name='SSH-ключ')
+
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.name
