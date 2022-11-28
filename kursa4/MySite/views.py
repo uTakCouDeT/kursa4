075330555ea1b6d162be6dcdef7b49fe3531cc3b
uTakCouDeT <b20030021@gmail.com>
2022-11-28 22:36:40 +0300

    ъ

--- kursa4/MySite/views.py
+++ kursa4/MySite/views.py
@@ -3,37 +3,45 @@ from .models import *
 
 
 def index(request):
+    context = {'title': 'Главная', 'cssFile': 'css/Главная.css'}
     return render(
         request,
         'Главная.html',
+        context=context,
     )
 
 
 def about(reqest):
+    context = {'title': 'О нас', 'cssFile': 'css/О-нас.css'}
     return render(
         reqest,
-        'О-нас.html'
+        'О-нас.html',
+        context=context,
     )
 
 
 def contacts(reqest):
+    context = {'title': 'Контакты', 'cssFile': 'css/Контакты.css'}
     return render(
         reqest,
         'Контакты.html',
+        context=context,
     )
 
 
 def vmCreate(reqest):
+    context = {'title': 'Создание виртуальных машин', 'cssFile': 'css/Создание-виртуальных-машин.css'}
     return render(
         reqest,
         'Создание-виртуальных-машин.html',
+        context=context,
     )
 
 
 def vmList(reqest):
-    context = {'all_VM': VirtualMachine.objects.all()}
+    context = {'title': 'Список виртуальных машин', 'cssFile': 'css/Список-машин.css', 'all_VM': VirtualMachine.objects.all()}
     return render(
         reqest,
         'Список-машин.html',
-        context=context
+        context=context,
     )
